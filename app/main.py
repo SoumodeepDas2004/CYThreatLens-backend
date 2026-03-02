351bf4c0565ad7195072f698039e31b2a9f4cb4c
SoumodeepDas2004 <higherdas09@gmail.com>
2026-03-02 10:24:03 +0530

    added cors policy

--- app/main.py
+++ app/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app import models  # ← ADD THIS LINE
 from app.routers import scan
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI(title="CYThreatLens API")
 
 Base.metadata.create_all(bind=engine)
