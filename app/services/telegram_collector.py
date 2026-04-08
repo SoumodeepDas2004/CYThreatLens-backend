3ec757d7d87e6ed8a7af7f6a0f217d9be8a4efdb
SoumodeepDas2004 <higherdas09@gmail.com>
2026-04-08 14:47:47 +0530

    telegram fix

--- app/services/telegram_collector.py
+++ app/services/telegram_collector.py
@@ -2,7 +2,15 @@ from telethon import TelegramClient
 import os
 import time
 from app.services.event_extractor import extract_event
-from datetime import datetime, timedelta,timezone
+from datetime import datetime, timedelta, timezone
+import base64
+
+# Load session (only once)
+session_data = os.getenv("SESSION_DATA")
+
+if session_data and not os.path.exists("cythreatlens_session.session"):
+    with open("cythreatlens_session.session", "wb") as f:
+        f.write(base64.b64decode(session_data))
 
 api_id = int(os.getenv("TELE_CLND_ID"))
 api_hash = os.getenv("TELE_HASH")
@@ -15,63 +23,54 @@ channels = [
     "cyintel01"
 ]
 
-CACHE_TTL = 600  # 10 minutes
+CACHE_TTL = 600
 
 telegram_cache = {
     "data": [],
     "last_update": 0
 }
 
-
 async def fetch_telegram_messages():
 
     now = time.time()
 
-    # Return cached data if within TTL
     if now - telegram_cache["last_update"] < CACHE_TTL:
         return telegram_cache["data"]
 
     results = []
 
-    async with TelegramClient(
-        "cythreatlens_session",
-        api_id,
-        api_hash
-    ) as client:
+    client = TelegramClient("cythreatlens_session", api_id, api_hash)
+    await client.connect()
 
-        # Loop through each channel
-        for channel in channels:
+    for channel in channels:
+        async for message in client.iter_messages(channel, limit=5):
 
-            async for message in client.iter_messages(channel, limit=5):
+            if message.text:
+                post_url = f"https://t.me/{channel}/{message.id}"
 
-                
+                if message.date < datetime.now(timezone.utc) - timedelta(days=2):
+                    continue
 
-                if message.text:
-                    #print("RAW MESSAGE:", message.text)
-                    # print("collecting new data from telegram")
-                    post_url = f"https://t.me/{channel}/{message.id}"
-                    # if message.date < datetime.utcnow() - timedelta(days=2):
-                    if message.date < datetime.now(timezone.utc) - timedelta(days=2):
-                        continue
-                    event = extract_event({
-                        "title": message.text,
-                        "source": channel,
-                        "url": post_url,
-                        "date": message.date.isoformat()
-                    })
-                    # event = extract_event({
-                    #     "title": message.text,
-                    #     "source": channel,
-                    #     "date": message.date.isoformat()
-                    # })
-                    # print("Collected returning data")
-                    if event:
-                        results.append(event)
+                event = extract_event({
+                    "title": message.text,
+                    "source": channel,
+                    "url": post_url,
+                    "date": message.date.isoformat()
+                })
+
+                if event:
+                    results.append(event)
+
+    await client.disconnect()
 
     telegram_cache["data"] = results
     telegram_cache["last_update"] = now
-    print("Collected returning data")
+
     return results
+
+
+# async def fetch_telegram_savedmessages():
+#     return telegram_cache["data"]
 
 async def fetch_telegram_savedmessages():
     if(telegram_cache["data"]):return telegram_cache["data"]
